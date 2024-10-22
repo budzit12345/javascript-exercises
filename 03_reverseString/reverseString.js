58e00fb4a912d404621c4eb0bbbddb88aafc1ada
budzit12345 <budzit12345@gmail.com>
2024-10-22 23:04:56 +0200

    added for loop to reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = "";
+    for(let i = string.length - 1; i >= 0; i--) {
+        reverse += string[i];
+    }
+    return reverse;
+    
 
-};
+}
+ reverseString('hello')
+
+;
 
 // Do not edit below this line
 module.exports = reverseString;
