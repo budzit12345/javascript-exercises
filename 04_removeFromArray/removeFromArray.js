57b15f15a27331a1dccc5b94a8c42e18ab29c438
budzit12345 <budzit12345@gmail.com>
2024-10-23 17:22:23 +0200

    while loop to iterate thought the array and remove values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    var i = 0;
+    while(i < array.length) {
+        if(num.includes(array[i])) {
+            array.splice(i, 1);  
+        } else {
+            i++;
+        }
+    }
+    return array;
+
 };
 
 // Do not edit below this line
