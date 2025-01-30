4bd238e8de9cc8b7fe29230a575276638a3c2e46
budzit12345 <budzit12345@gmail.com>
2025-01-30 21:38:10 +0100

    only finisghed first test, very hard ;/

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(x,num) {
+    y = [];
+
+    for(let i = 0;i < x.length;i++) {
+        if(x[i] !== num) {
+            y.push(x[i]); 
+        }
+        else {
+            continue
+        }
+    }
+    return y;
+
+
 };
 
 // Do not edit below this line
