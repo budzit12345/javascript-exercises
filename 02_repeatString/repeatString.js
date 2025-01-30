e0c1cdca33e46bd8934fc728b61040be6d34c29f
budzit12345 <budzit12345@gmail.com>
2025-01-30 20:51:01 +0100

    made a loop whhich adds string, finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let temporary = ''
+
+    if(num < 0) {
+        return 'ERROR'
+    }
+    else {
+        for(let i = 0; i < num;i++) {
+            temporary += string
+        }
+        return temporary;
+    }
 
 };
 
