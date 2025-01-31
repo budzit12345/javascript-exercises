4306f01ffa96168b25562d2c7f5ad3190452b988
budzit12345 <budzit12345@gmail.com>
2025-01-31 20:36:59 +0100

    done with the last exercise!!!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  let toCelcius = (n - 32) * 5/9
+  if(toCelcius % 1 !== 0) {
+    return Number(toCelcius.toFixed(1))
+  }
+  else {
+    return toCelcius
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let toFahrn = (x * 9/5 + 32) 
+  if(toFahrn % 1 !== 0) {
+    return Number(toFahrn.toFixed(1))
+  }
+  else {
+    return toFahrn
+  }
 };
 
 // Do not edit below this line
