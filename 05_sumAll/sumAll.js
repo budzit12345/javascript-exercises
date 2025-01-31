11563614d8c4fb99e26e1576b1898fae21c772e6
budzit12345 <budzit12345@gmail.com>
2025-01-31 14:37:28 +0100

    easyyy, done with it, previous was wayy harder

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let suma = 0;
+    if(x < 0 || y < 0 || typeof x !== "number" || typeof y !== "number" || x % 1 !== 0 || y % 1 !== 0 ){
+        return "ERROR"
+    }
+    else if (x > y) {
+        for(let i = y;i <= x;i++) {
+            suma += i;
+        }
+        return suma
+    }
+    else {
+        for(let j = x;j <= y;j++) {
+            suma += j;
+        }
+        return suma;
+    }
+
 
 };
 
