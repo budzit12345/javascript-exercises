58f79fa51a4631f555d4db45d6acea2d2c3b4d8a
budzit12345 <budzit12345@gmail.com>
2025-02-26 10:17:08 +0100

    done with the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let costam = str.split(" ");
+    let gowno = costam.join('').toLowerCase();
+    let huj = gowno.replace(/[^a-zA-Z0-9]/g, '');
+    
+    let hujReversed = '';
+    for(let i = huj.length - 1;i >= 0;i--) {
+        hujReversed += huj[i]
+    }
 
+    if(huj === hujReversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
