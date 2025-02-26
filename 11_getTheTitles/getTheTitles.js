36c8fac214a56a231f279dab2afcfb7abf944fcc
budzit12345 <budzit12345@gmail.com>
2025-02-26 18:48:24 +0100

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let kupka = obj.map((ksiazka) => ksiazka.title)
+    return kupka;
 };
 
 // Do not edit below this line
