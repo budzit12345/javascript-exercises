9fcac8c31ccc9820c0f6f06b5da69fc3e9ec6a46
budzit12345 <budzit12345@gmail.com>
2025-02-26 18:38:28 +0100

    done with the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
-};
+const fibonacci = (n) => {
+    // Zamiana stringa na liczbę
+    n = Number(n);
+  
+    // Sprawdzenie, czy liczba jest ujemna
+    if (n < 0) return "OOPS";
+  
+    // Obsługa przypadku n == 0
+    if (n === 0) return 0;
+  
+    // Pierwsze dwa wyrazy ciągu Fibonacciego
+    let a = 1, b = 1;
+  
+    // Obliczanie wartości dla n >= 3
+    for (let i = 3; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+  
+    return b;
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
